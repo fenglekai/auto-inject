03ed46426bdf742bb3c20dc1e4c6941d19d628d0
fenglekai <674640130@qq.com>
2023-10-24 16:05:03 +0800

    feat(mongo): 更新mongo连接与断开功能

--- src/koa/service/mongoDBService.ts
+++ src/koa/service/mongoDBService.ts
@@ -1,10 +1,5 @@
 import { MongoClient } from 'mongodb'
 
-interface ConnectParams {
-  ip: string
-  port: number
-}
-
 export class MongoDBClient {
   client: MongoClient | null
   constructor() {
@@ -15,48 +10,56 @@ export class MongoDBClient {
     return ip.replace('localhost', '127.0.0.1')
   }
 
-  connect = async (params: ConnectParams) => {
+  connect = async (url: string) => {
     // Connection URL
-    const url = `mongodb://${this.localhostReplace(params.ip)}:${params.port}`
-    this.client = new MongoClient(url)
+    const mongoUrl = `mongodb://${this.localhostReplace(url)}`
+    this.client = new MongoClient(mongoUrl)
 
     // Use connect method to connect to the server
     await this.client.connect()
-    console.log('Connected successfully to mongo server')
+    return 'Connected successfully to mongo server'
   }
 
-  find = async (DBName: string,conName: string,data: any = {}) => {
+  disconnect = async () => {
     if (this.client) {
-        const collection = this.client.db(DBName).collection(conName)
-        const result = await collection.find(data).toArray();
-        return result
+      await this.client.close()
+      return 'Successfully disconnected from mongo server'
     }
     throw Error('Client is not connected')
   }
 
-  insert = async (DBName: string,conName: string,data: any = []) => {
+  find = async (DBName: string, tabName: string, data: any = {}) => {
     if (this.client) {
-        const collection = this.client.db(DBName).collection(conName)
-        const result = await collection.insertMany(data);
-        return result
+      const collection = this.client.db(DBName).collection(tabName)
+      const result = await collection.find(data).toArray()
+      return result
     }
     throw Error('Client is not connected')
   }
 
-  update = async (DBName: string,conName: string,data: any = {},setData: {}) => {
+  insert = async (DBName: string, tabName: string, data: any = []) => {
     if (this.client) {
-        const collection = this.client.db(DBName).collection(conName)
-        const result = await collection.updateMany(data, { $set: setData });
-        return result
+      const collection = this.client.db(DBName).collection(tabName)
+      const result = await collection.insertMany(data)
+      return result
     }
     throw Error('Client is not connected')
   }
 
-  remove = async (DBName: string,conName: string,data: any = {}) => {
+  update = async (DBName: string, tabName: string, data: any = {}, setData: {}) => {
     if (this.client) {
-        const collection = this.client.db(DBName).collection(conName)
-        const result = await collection.deleteMany(data);
-        return result
+      const collection = this.client.db(DBName).collection(tabName)
+      const result = await collection.updateMany(data, { $set: setData })
+      return result
+    }
+    throw Error('Client is not connected')
+  }
+
+  remove = async (DBName: string, tabName: string, data: any = {}) => {
+    if (this.client) {
+      const collection = this.client.db(DBName).collection(tabName)
+      const result = await collection.deleteMany(data)
+      return result
     }
     throw Error('Client is not connected')
   }
